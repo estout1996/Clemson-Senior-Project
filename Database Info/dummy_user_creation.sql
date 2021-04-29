112d041f5044b65bd0ce85fa63d82b313fdfb6fe
estout1996 <56238561+estout1996@users.noreply.github.com>
2021-04-29 13:47:58 -0400

    Add files via upload

---
+++ Database Info/dummy_user_creation.sql
@@ -0,0 +1,29 @@
+USE application_db;
+/*
+INSERT INTO Company (CompanyName, PointToDollar, Email, OfficePhone)
+VALUES ('Dummy Company', 100, 'company@gmail.com', '(123) 456-7890');
+
+INSERT INTO Users (UserType, Username, FirstName, MiddleName, LastName, PreferredName, UserPassword)
+VALUES ('D', 'driver@gmail.com', 'Dummy', 'Driver', 'Account', 'DDA', 'Password1@');
+
+INSERT INTO Driver (Address, UserID)
+VALUES ('105 Sikes Hall, Clemson, SC 29634', 1);
+
+INSERT INTO Users (UserType, Username, FirstName, MiddleName, LastName, PreferredName, UserPassword)
+VALUES ('S', 'sponsor@gmail.com', 'Dummy', 'Sponsor', 'Account', 'DSA', 'Password1@');
+
+INSERT INTO Sponsor (CompanyID, UserID)
+VALUES (1, 2);
+
+INSERT INTO Users (UserType, Username, FirstName, MiddleName, LastName, PreferredName, UserPassword)
+VALUES ('A', 'admin@gmail.com', 'Dummy', 'Admin', 'Account', 'DAA', 'Password1@');
+
+INSERT INTO Users (UserType, Username, FirstName, MiddleName, LastName, PreferredName, UserPassword)
+VALUES ('A', 'mfgresk@g.clemson.edu', 'Michael', 'N/A', 'N/A', 'Michael', 'Password1@');
+*/
+
+-- INSERT INTO DriverPoints (DriverID, CompanyID, Points)
+-- VALUES (1, 2, 100);
+
+-- INSERT INTO Company (CompanyName, PointToDollar, Email, OfficePhone)
+-- VALUES ('Dummy Company 7', 100, 'company6@gmail.com', '(123) 456-8887');
