bf9c4109e2b180a583f37d98a82bbe4b0e808631
estout1996 <56238561+estout1996@users.noreply.github.com>
2021-04-29 13:51:43 -0400

    Add files via upload

---
+++ Database Info/test_queries.sql
@@ -0,0 +1,25 @@
+USE final_application_db;
+
+SELECT * FROM Users;
+
+SELECT * FROM Driver;
+
+SELECT * FROM Sponsor;
+
+SELECT * FROM LoginAttempts;
+
+SELECT * FROM PasswordChange;
+
+SELECT * FROM Company;
+
+SELECT * FROM CatalogItems;
+
+SELECT * FROM CatalogCart;
+
+SELECT * FROM DriverPoints;
+
+SELECT * FROM DriverApplications;
+
+SELECT * FROM CatalogPurchases;
+
+SELECT * FROM PointChanges;
